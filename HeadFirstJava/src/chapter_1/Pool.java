b10eaff52cfbb40f9062791f3684b9108b35ee38
alexey_udalov <asudalov@vtb.ru>
2024-01-18 14:41:47 +0300

    chapter_1[Pool]: add Pool class

---
+++ HeadFirstJava/src/chapter_1/Pool.java
@@ -0,0 +1,31 @@
+package chapter_1;
+
+public class Pool {
+    public static void main(String[] args) {
+        int x = 0;
+
+        while (x < 4) {
+
+            System.out.print("a");
+
+            if (x < 1) {
+                System.out.print(" ");
+            }
+
+            x = x + 1;
+
+            if (x > 0) {
+                System.out.println("noise");
+
+            }
+            if (x == 1) {
+
+            }
+            if () {
+
+            }
+            System.out.println("");
+
+        }
+    }
+}
